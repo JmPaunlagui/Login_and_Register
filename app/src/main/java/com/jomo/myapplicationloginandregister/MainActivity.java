9295a6ff2198f8dc6d83a30ae0b6a9adea4f99ab
JmPaunlagui <43374749+JmPaunlagui@users.noreply.github.com>
2020-01-02 16:17:27 +0800

    Just moving files

--- app/src/main/java/com/jomo/myapplicationloginandregister/MainActivity.java
+++ app/src/main/java/com/jomo/myapplicationloginandregister/MainActivity.java
@@ -2,7 +2,9 @@ package com.jomo.myapplicationloginandregister;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //For translucent status bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
     }
 }
